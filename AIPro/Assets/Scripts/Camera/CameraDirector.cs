3d52111334d9ff5395f7629e5d1bce7e970ef174
Yukihamu <tyoko.tennis.rapido@outlook.jp>
2019-11-20 14:46:12 +0900

    2つのカメラ切り替え機能追加【スペースキーで切り替え】

---
+++ AIPro/Assets/Scripts/Camera/CameraDirector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDirector : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject firstPersonCamera;
+    [SerializeField]
+    private GameObject thirdPersonCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // スペースキーでカメラを切り替える
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // ↓現在のactive状態から反転 
+            firstPersonCamera.SetActive(!firstPersonCamera.activeInHierarchy);
+            thirdPersonCamera.SetActive(!thirdPersonCamera.activeInHierarchy);
+        }
+    }
+}
