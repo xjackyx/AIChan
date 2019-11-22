e831adf0df0e422882f957d480d840e9ca94af66
xjackyx <57517392+xjackyx@users.noreply.github.com>
2019-11-22 14:58:48 +0900

    1~13選択機能追加(完成版じゃない)
    QとEキー(左右回転)
    
    元0番(真ん中)
    Ｌ押すと+1番(二番目になる)
    13番まで

--- AIPro/Assets/Scripts/PlayerCamera.cs
+++ AIPro/Assets/Scripts/PlayerCamera.cs
@@ -14,20 +14,12 @@ public class PlayerCamera : MonoBehaviour
         // ここに毬栗ゲーで作った感じにカメラのやつやってけばいいと思う。
         this.player = GameObject.Find("Player");
 
-        Vector3 playerPos = this.player.transform.position;
-        transform.position = new Vector3(playerPos.x, playerPos.y + OFFSET_POS_Y, playerPos.z - 13.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.K))
-        {
-            // カメラの移動
-            transform.Translate(0, 0.1f, 0);
-
-        }
-
+        
         Vector3 playerPos = this.player.transform.position;
         transform.position = new Vector3(playerPos.x, playerPos.y + OFFSET_POS_Y, playerPos.z - 13.0f);
 
--- AIPro/Assets/Scripts/Rotation.cs
+++ AIPro/Assets/Scripts/Rotation.cs
@@ -2,25 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class Rotation : MonoBehaviour
 {
+    //private bool RotationActive = false;
 
 
     // Update is called once per frame
     void Update()
     {
-  
-        // Right Rotate
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+
+
+        RotateRight();
+        RotateLeft();
+
+
+        //if(BO[1] == true)
+        //{
+        //    GetComponent<Rotation>().enabled = true;
+
+
+        //    // Right Rotate
+        //    RotateRight();
+
+        //    // Left Rotate
+        //    RotateLeft();
+        //}
+        //else
+        //{
+        //    GetComponent<Rotation>().enabled = false;
+        //}
+
+    }
+
+    public void RotateRight()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             transform.Rotate(Vector3.up, 60);
         }
+    }
 
-        // Left Rotate
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+    public void RotateLeft()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
         {
             transform.Rotate(Vector3.up, -60);
         }
-
     }
+
 }
