78d3b9e79f6f6c78f41d6b01798a109566dd0a01
Yuki <rei22.lucky.tennis@icloud.com>
2019-12-22 07:28:57 +0900

    プレイヤー移動、向き、カメラ移動、向き完成//

--- AIChan_Master_LRP/Assets/Scripts/Game_Scripts/Player/PlayerOrientation.cs
+++ AIChan_Master_LRP/Assets/Scripts/Game_Scripts/Player/PlayerOrientation.cs
@@ -12,6 +12,7 @@ public class PlayerOrientation : MonoBehaviour
 
 
     private Vector3 Player_pos; //プレイヤーのポジション
+    public float PlayerOrientarion;
     private float x; //x方向のImputの値
     private float z; //z方向のInputの値
     private Rigidbody rigd;
@@ -20,6 +21,7 @@ public class PlayerOrientation : MonoBehaviour
     {
         Player_pos = GetComponent<Transform>().position; //最初の時点でのプレイヤーのポジションを取得
         rigd = GetComponent<Rigidbody>(); //プレイヤーのRigidbodyを取得
+        PlayerOrientarion = 0.7f;
     }
 
     void Update()
@@ -29,14 +31,20 @@ public class PlayerOrientation : MonoBehaviour
         z = Input.GetAxis("Vertical"); //z方向のInputの値を取得
 
 
-        // カメラの方向から、X-Z平面の単位ベクトルを取得
-        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        //// カメラの方向から、X-Z平面の単位ベクトルを取得
+        //Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
 
-        // 方向キーの入力値とカメラの向きから、移動方向を決定
-        Vector3 moveForward = cameraForward * inputVertical + Camera.main.transform.right * inputHorizontal;
+        //// 方向キーの入力値とカメラの向きから、移動方向を決定
+        //Vector3 moveForward = cameraForward * x + Camera.main.transform.right * z;
 
-        // 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す。
-        rigd.velocity = moveForward * moveSpeed + new Vector3(x * 0.00001f, rigd.velocity.y, z * 0.00001f);
+        //// 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す。
+        //rigd.velocity = moveForward * moveSpeed + new Vector3(x, rigd.velocity.y, z);
+
+        //// キャラクターの向きを進行方向に
+        //if (moveForward != Vector3.zero)
+        //{
+        //    transform.rotation = Quaternion.LookRotation(moveForward);
+        //}
 
         //// キャラクターの向きを進行方向に
         //if (moveForward != Vector3.zero)
@@ -51,6 +59,9 @@ public class PlayerOrientation : MonoBehaviour
 
 
 
+/*
+■ゆっくり回転方法
+
         Vector3 diff = transform.position - Player_pos; //プレイヤーがどの方向に進んでいるかがわかるように、初期位置と現在地の座標差分を取得
 
         if (diff.magnitude > 0.00001f) //ベクトルの長さが0.01fより大きい場合にプレイヤーの向きを変える処理を入れる(0では入れないので）
@@ -63,5 +74,40 @@ public class PlayerOrientation : MonoBehaviour
         }
 
         //Player_pos = transform.position; //プレイヤーの位置を更新
+  */
     }
+
+    private void FixedUpdate()
+    {
+
+
+        // カメラの方向から、X-Z平面の単位ベクトルを取得
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+
+        // 方向キーの入力値とカメラの向きから、移動方向を決定
+        Vector3 moveForward = cameraForward * z + Camera.main.transform.right * x;
+
+        // 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す。
+        rigd.velocity = moveForward * moveSpeed + new Vector3(0, rigd.velocity.y, 0);
+
+
+        Vector3 diff = transform.position - Player_pos; //プレイヤーがどの方向に進んでいるかがわかるように、初期位置と現在地の座標差分を取得
+
+        // キャラクターの向きを進行方向に
+        //if (moveForward != Vector3.zero)
+        //{
+        if (diff.magnitude > 0.001f) //ベクトルの長さが0.01fより大きい場合にプレイヤーの向きを変える処理を入れる(0では入れないので）
+        {
+            // AIちゃん向きの取得。
+            Quaternion rotation = Quaternion.LookRotation(diff);
+
+            // AIちゃん元の向きと変化された向きから補間してクルクル回る。
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, PlayerOrientarion);
+
+            //transform.rotation = Quaternion.LookRotation(moveForward);
+        }
+        Player_pos = transform.position; //プレイヤーの位置を更新
+
+    }
+
 }
