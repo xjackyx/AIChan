090d5263a501f97de53fd2e6a1a1308eeb8c89d2
Yukihamu <tyoko.tennis.rapido@outlook.jp>
2020-01-20 12:45:10 +0900

    プレイヤーの座標にアイコンを配置

---
+++ AIChan_Master_LRP/Assets/Scripts/PlayerIcon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIcon : MonoBehaviour
+{
+    public GameObject _parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _parent = transform.root.gameObject;
+        Vector3 pos = _parent.transform.position;
+        transform.position = pos;
+    }
+}
